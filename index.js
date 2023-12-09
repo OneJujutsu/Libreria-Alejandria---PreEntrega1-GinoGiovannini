3d0cc37d14310dd4e04a4a81b7fd6f71d9a39737
Enzo Fernandez <enzitofernandez288@gmail.com>
2023-12-09 16:30:48 -0300

    Arreglando error bucle infinito

--- index.js
+++ index.js
@@ -20,46 +20,49 @@ if (bienvenido == "si" || bienvenido == "SI") {
     final = alert("Entendible que tenga buen dia.");
 }
 
-while (productos != "Manga" || productos != "Policial" || productos != "Terror" || productos != "Fantastico" || productos != "ninguno") {
-    alert("Lo siento pero no tenemos ese libro disponible, si quiere puede ver nuestra coleccion");
+while (true) {
+    alert("Lo siento pero no tenemos ese libro disponible, si quiere puede ver nuestra colección");
 
-    productos = prompt("Perfecto estos son todos los libros que tenemos disponible ahora mismo, cual te gustaria llevar           Manga, Policial, Terror, Fantasticos    ninguno");
-    if (productos == "Manga" || productos == "Policial" || productos == "Terror" || productos == "Fantastico"){
+    productos = prompt("Perfecto, estos son todos los libros que tenemos disponible ahora mismo. ¿Cuál te gustaría llevar? (Manga, Policial, Terror, Fantastico, ninguno)");
+
+    if (productos == "Manga" || productos == "Policial" || productos == "Terror" || productos == "Fantastico") {
         switch (productos) {
-            case Manga:
-                cantidaddeproductos = Number(prompt("Cuantas unidades de Manga le gustaria llevar?"))
-                total += cantidad(cantidaddeproductos, 2000)
+            case "Manga":
+                let cantidadManga = Number(prompt("¿Cuántas unidades de Manga le gustaría llevar?"));
+                total += cantidad(cantidadManga, 2000);
                 break;
 
-            case Policial:
-                cantidaddeproductos = Number(prompt("Cuantas unidades de Policial le gustaria llevar?"))
-                total += cantidad(cantidaddeproductos, 4000)
+            case "Policial":
+                let cantidadPolicial = Number(prompt("¿Cuántas unidades de Policial le gustaría llevar?"));
+                total += cantidad(cantidadPolicial, 4000);
                 break;
 
-            case Terror:
-                cantidaddeproductos = Number(prompt("Cuantas unidades de Terror le gustaria llevar?"))
-                total += cantidad(cantidaddeproductos, 500)
+            case "Terror":
+                let cantidadTerror = Number(prompt("¿Cuántas unidades de Terror le gustaría llevar?"));
+                total += cantidad(cantidadTerror, 500);
                 break;
 
-            case Fantastico:
-                cantidaddeproductos = Number(prompt("Cuantas unidades de Fantastico le gustaria llevar?"))
-                total += cantidad(cantidaddeproductos, 6000)
+            case "Fantastico":
+                let cantidadFantastico = Number(prompt("¿Cuántas unidades de Fantastico le gustaría llevar?"));
+                total += cantidad(cantidadFantastico, 6000);
                 break;
 
             default:
-                satisfecho = prompt ("Está satisfecho con lo que tiene en su carrito o quisiera agregar algo más          (estoy satisfecho)  (quisiera agregar algo mas)")
+                satisfecho = prompt("¿Está satisfecho con lo que tiene en su carrito o quisiera agregar algo más? (estoy satisfecho) (quisiera agregar algo mas)");
                 break;
-                
-        }if(satisfecho == "estoy satisfecho"){
-            alert("su total a pagar es de :" + total)
-        }else if (satisfecho == "quisiera agregar algo mas"){
-            productos = prompt("Perfecto estos son todos los libros que tenemos disponible ahora mismo, cual te gustaria llevar           Manga, Policial, Terror, Fantasticos    ninguno");
         }
-    }else if (productos == "ninguno"){
-        final = alert("Entendible que tenga buen dia.");
+
+        if (satisfecho == "estoy satisfecho") {
+            alert("Su total a pagar es de: " + total);
+            break;
+        } else if (satisfecho == "quisiera agregar algo mas") {
+            // Continuar con el bucle
+        }
+    } else if (productos == "ninguno") {
+        alert("Entendible que tenga buen día.");
+        break;
     }
 }
 
 
-
-//alert("Gracias por su compra");
+alert("Gracias por su compra");
